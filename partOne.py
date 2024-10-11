fe9a180901d28212fddb0ec134158100011b2014
b3smooth <bas43@kent.ac.uk>
2024-10-11 09:19:04 +0000

    V1 with loop

--- partOne.py
+++ partOne.py
@@ -1,6 +1,7 @@
 age = int(input("please input your age"))
 if age > 0 and age<18:
     print("you are a child")
-else:
+elif(age>0 and age>=18):
     print("You are an adult")
-
+else:
+    print("you are not possible")
--- partTwo.py
+++ partTwo.py
@@ -1,1 +1,17 @@
+import random
+truefalse = False
+secret_number = random.randint(1,10)
+print(secret_number)
+while truefalse==False:
+    UInput = int(input("Please input a new number"))
+    if(UInput==secret_number):
+        print("just right")
+        truefalse=True
+    elif(UInput>secret_number):
+        print("Too high")
+    else:
+        print("Too low")
+
+
+
 
