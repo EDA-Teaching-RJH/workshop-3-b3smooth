72230ebe7f9efa187e1f730b49ce1cf77f309750
b3smooth <bas43@kent.ac.uk>
2024-10-11 09:57:04 +0000

    v2 p5

--- partFive.py
+++ partFive.py
@@ -2,5 +2,7 @@
 UDay= input("please enter in a day of the week ")
 if (UDay=="Saturday"or UDay=="Sunday"):
     print("It's the weekend!")
-else:
+elif(UDay=="Monday" or UDay=="Tuesday" or UDay=="Wednesday" or UDay=="Thursday" or UDay=="Friday"):
     print("It's a weekday")
+else:
+    print("That's not a day of the week")
