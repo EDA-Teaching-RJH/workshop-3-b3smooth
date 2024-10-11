9f7ffc5c16d90dc3cf642765b1271bc4d6e85aef
b3smooth <bas43@kent.ac.uk>
2024-10-11 10:24:49 +0000

    v1 P7

--- partSeven.py
+++ partSeven.py
@@ -1,1 +1,34 @@
+TF= False
+while TF==False:
+    U1=float(input("Please enter in your first number "))
+    U2= float(input("Please enter in your second number "))
+    UOperator=input("Please enter in an operator ")
+    match UOperator:
+        case "+":
+            Result = U1 + U2
+            print(f"{U1} + {U2} = {Result}")
+        case "-":
+            Result = U1-U2
+            print(f"{U1} - {U2} = {Result}")
+        case "*":
+            Result = U1*U2
+            print(f"{U1} * {U2} = {Result}")
+        case "/":
+            if(U1!=0 and U2!=0):
+                Result = U1/U2
+                print(f"{U1} * {U2} = {Result}")
+            else:
+                print("this is a divide by 0 error")
+        case "^":
+            Result = U1**U2
+            print(f"{U1} ^ {U2} = {Result}")
+        case "%":
+            Result = U1%U2
+            print(f"{U1} % {U2} = {Result}")
+        case "quit":
+            print("The application is ending")
+            TF=True
+            break
+
+
 
