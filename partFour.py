ce0dce4690c734acd9b282774791d242dda7247d
b3smooth <bas43@kent.ac.uk>
2024-10-11 09:39:01 +0000

    V1 P4 password

--- partFour.py
+++ partFour.py
@@ -1,1 +1,8 @@
-
+AUName="admin"
+APassword="password123"
+Uinput=input("Please enter in your username ")
+UPassword= input("Please enter in your password ")
+if(Uinput==AUName and UPassword==APassword):
+    print("Access allowed")
+else:
+    print("Error Access Denied")
