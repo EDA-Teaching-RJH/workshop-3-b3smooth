be8b49e92383f3a2854b5b0f731acd5c9d688c53
b3smooth <bas43@kent.ac.uk>
2024-10-11 09:53:13 +0000

    V1 Ticket price p6

--- partSix.py
+++ partSix.py
@@ -1,1 +1,8 @@
-
+UAge= int(input("Please input in your age "))
+UStu= input("are you a student yes/no ")
+if (UAge<12 or UAge>65):
+    print("£5")
+elif (UAge>12 or UAge<65)and UStu=="yes":
+    print("£8")
+else:
+    print("£10")
